b7023b789e7db50ca3424b234c9041deb96e5f84
Radu F <filipradu24@gmail.com>
2017-11-25 16:09:50 +0200

    Upload image endpoint

---
+++ Parker/Parker/Controllers/RecieveFilesController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.IO;
+
+namespace Parker.Controllers
+{
+
+    [Produces("application/json")]
+    [Route("api/RecieveFiles")]
+    public class RecieveFilesController : Controller
+    {
+        [HttpPost("{location}")]
+        public IActionResult Post(string location)
+        {
+            var file = Request.Form.Files[0];
+
+            var currentDirectory = Directory.GetCurrentDirectory();
+            var filePath = currentDirectory + "/Photos/" + location +"/"+ file.FileName;
+            using (var stream = new FileStream(filePath, FileMode.Create))
+            {
+                file.CopyToAsync(stream);
+            }
+
+            return Ok();
+        }
+    }
+}
